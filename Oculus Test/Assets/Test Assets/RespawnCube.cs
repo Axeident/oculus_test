bd5b49cf8f74f0a2a846a36f80992abb399532f6
axeident <hmanlick@gmail.com>
2019-11-06 11:48:06 -0500

    ready for test 2
    multiple smaller cubs that can be picked up
    respawn box for thrwoing

---
+++ Oculus Test/Assets/Test Assets/RespawnCube.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnCube : MonoBehaviour
+{
+    private Transform spawnPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnPoint = transform.transform;
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.CompareTag("Respawn"))
+        {
+            this.transform.position = spawnPoint.position;
+        }
+    }
+}
